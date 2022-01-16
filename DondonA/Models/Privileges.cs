e3ecd2c58fbf2dbd4035befbfb5fb9b96f4a2d64
Hajcik <hajcik666pl@gmail.com>
2022-01-16 22:13:42 +0100

    Deleted project

--- DondonA/Models/Privileges.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DondonA.Models
-{
-    public class Privileges
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
